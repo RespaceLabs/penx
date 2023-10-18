f0109130e0f630ef71cfed70b4bdda8b59e77c30
0xzion <0xyz.penx@gmail.com>
2023-10-18 22:14:21 +0800

    feat: create get github token api

--- packages/model/src/User.ts
+++ packages/model/src/User.ts
@@ -1,5 +1,13 @@
 import { User as IUser } from '@prisma/client'
 
+type GhConnectionInfo = Record<
+  string,
+  {
+    repoName: string
+    installationId: number
+  }
+>
+
 export class User {
   constructor(public raw: IUser) {}
 
@@ -7,11 +15,19 @@ export class User {
     return this.raw.address
   }
 
-  get ghConnectionInfo() {
+  get ghConnectionInfo(): GhConnectionInfo {
     return JSON.parse(this.raw.ghConnectionInfo || '{}')
+  }
+
+  getSpace(spaceId: string) {
+    return this.ghConnectionInfo[spaceId]
   }
 
   getRepoName(spaceId: string) {
     return this.ghConnectionInfo[spaceId]?.repoName
+  }
+
+  getInstallationId(spaceId: string) {
+    return this.ghConnectionInfo[spaceId]?.installationId
   }
 }
