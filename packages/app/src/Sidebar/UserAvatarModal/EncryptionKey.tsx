5f0b41df0544d5f012aeb180146d87c43066c403
0xzion <0xyz.penx@gmail.com>
2023-10-28 16:28:04 +0800

    feat: can custom encryption key

--- packages/app/src/Sidebar/UserAvatarModal/EncryptionKey.tsx
+++ packages/app/src/Sidebar/UserAvatarModal/EncryptionKey.tsx
@@ -1,12 +1,26 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Box } from '@fower/react'
-import { Eye } from 'lucide-react'
-import { Button } from 'uikit'
+import { get, set } from 'idb-keyval'
+import { Check, Eye, PencilLine } from 'lucide-react'
+import { nanoid } from 'nanoid'
+import { Button, Input } from 'uikit'
 
 interface Props {}
 
+const ENCRYPTION_KEY = 'ENCRYPTION_KEY'
+
 export const EncryptionKey: FC<Props> = () => {
   const [blur, setBlur] = useState(true)
+  const [editing, setEditing] = useState(false)
+  const [key, setKey] = useState('')
+
+  useEffect(() => {
+    get(ENCRYPTION_KEY).then((value) => {
+      if (value) return setKey(value)
+      setKey(nanoid())
+    })
+  }, [])
+
   return (
     <Box column gap2>
       <Box textLG fontSemibold>
@@ -15,16 +29,7 @@ export const EncryptionKey: FC<Props> = () => {
       <Box gray600 leadingNormal textSM>
         The key to encrypt you data, please remember it, and don't forget it!
       </Box>
-      <Box
-        bgZinc100
-        px4
-        py3
-        relative
-        rounded2XL
-        toBetween
-        toCenterY
-        onClick={() => setBlur(!blur)}
-      >
+      <Box bgZinc100 px4 relative rounded2XL toBetween toCenterY gap2 h-60>
         {blur && (
           <Box
             absolute
@@ -35,7 +40,42 @@ export const EncryptionKey: FC<Props> = () => {
             style={{ backdropFilter: 'blur(5px)' }}
           />
         )}
-        <Box>sexerwdrerwwdfsrwerdderersserwe</Box>
+        {!editing && <Box flex-1>{key}</Box>}
+        {editing && (
+          <Input
+            flex-1
+            value={key}
+            onChange={async (e) => {
+              setKey(e.target.value)
+              await set(ENCRYPTION_KEY, e.target.value)
+            }}
+          />
+        )}
+
+        {editing && (
+          <Button
+            isSquare
+            size="sm"
+            variant="light"
+            colorScheme="gray500"
+            onClick={() => setEditing(!editing)}
+          >
+            <Check />
+          </Button>
+        )}
+
+        {!editing && (
+          <Button
+            isSquare
+            size="sm"
+            variant="light"
+            colorScheme="gray500"
+            onClick={() => setEditing(!editing)}
+          >
+            <PencilLine />
+          </Button>
+        )}
+
         <Button
           relative
           isSquare
