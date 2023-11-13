aa4dca36590ce36b24c59c5c3e7d7bcda1f4441d
0xzion <0xyz.penx@gmail.com>
2023-11-14 00:12:17 +0800

    feat: can sync 101

---
+++ packages/hooks/src/useWorkers/syncPenx101.ts
@@ -0,0 +1,47 @@
+import ky from 'ky'
+import { db } from '@penx/local-db'
+import { INode } from '@penx/model-types'
+import { sleep } from '@penx/shared'
+
+// const INTERVAL = 5 * 60 * 1000
+const INTERVAL = 5 * 1000
+
+let isPolling = true
+
+export async function syncPenx101() {
+  while (isPolling) {
+    await sync()
+    await sleep(INTERVAL)
+  }
+}
+
+async function sync() {
+  console.log('sync......101')
+
+  try {
+    const url =
+      'https://raw.githubusercontent.com/penx-lab/penx-101/main/nodes.json'
+
+    const data: INode[] = await ky(url).json()
+
+    console.log('data:', data)
+
+    const nodes = await db.listNodesBySpaceId('penx-101')
+    for (const node of nodes) {
+      await db.node.deleteByPk(node.id)
+    }
+
+    for (const item of data) {
+      // const find = await db.getNode(item.id)
+      // console.log('find:', find)
+      await db.createNode({
+        ...item,
+        spaceId: 'penx-101',
+      })
+    }
+
+    console.log('sync 101 done')
+  } catch (error) {
+    console.log('error nodes:', error)
+  }
+}
--- packages/hooks/src/useWorkers/worker.ts
+++ packages/hooks/src/useWorkers/worker.ts
@@ -1,6 +1,7 @@
 import { WorkerEvents } from '@penx/constants'
 import { db } from '@penx/local-db'
 import { clearNodes } from './clearNodes'
+import { syncPenx101 } from './syncPenx101'
 import { updateExtension } from './updateExtension'
 
 self.addEventListener('message', async (event) => {
@@ -12,4 +13,6 @@ self.addEventListener('message', async (event) => {
 
   // updateExtension()
   await clearNodes()
+
+  await syncPenx101()
 })
