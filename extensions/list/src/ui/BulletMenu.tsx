a985e4a4bcb24e145e3dcfa0e0aacce6b6d6b861
0xzion <0xyz.penx@gmail.com>
2023-12-13 11:23:53 +0800

    feat: can copy node ID

--- extensions/list/src/ui/BulletMenu.tsx
+++ extensions/list/src/ui/BulletMenu.tsx
@@ -1,7 +1,9 @@
 import { Node, Path, Transforms } from 'slate'
+import { toast } from 'uikit'
 import { ContextMenu, MenuItem } from '@penx/context-menu'
 import { useEditorStatic } from '@penx/editor-common'
 import { findNodePath } from '@penx/editor-queries'
+import { useCopyToClipboard } from '@penx/shared'
 import { store } from '@penx/store'
 import { ListContentElement } from '../types'
 
@@ -13,11 +15,17 @@ interface Props {
 export const BulletMenu = ({ menuId, element }: Props) => {
   const editor = useEditorStatic()
   const path = findNodePath(editor, element)!
+  const { copy } = useCopyToClipboard()
 
   function handleItemClick(type: string) {
     if (type === 'DELETE') {
       Transforms.removeNodes(editor, { at: Path.parent(path) })
     }
+  }
+
+  function copyNodeId() {
+    copy(element.id)
+    toast.info('Copied to clipboard')
   }
 
   async function openInNewPanel() {
@@ -31,7 +39,7 @@ export const BulletMenu = ({ menuId, element }: Props) => {
       {/* <MenuItem onClick={() => handleItemClick('a')}>Add to favorite</MenuItem> */}
       <MenuItem onClick={openInNewPanel}>Open in new panel</MenuItem>
       {/* <MenuItem onClick={() => handleItemClick('b')}>Publish</MenuItem> */}
-      <MenuItem onClick={() => handleItemClick('c')}>Copy</MenuItem>
+      <MenuItem onClick={copyNodeId}>Copy node ID</MenuItem>
       <MenuItem onClick={() => handleItemClick('DELETE')}>Delete</MenuItem>
       {/* <MenuItem onClick={() => handleItemClick('d')}>Expand all</MenuItem> */}
       {/* <MenuItem onClick={() => handleItemClick('d')}> Collapse all</MenuItem> */}
