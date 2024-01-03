025ef8436bac826de97d03e1189f987871762774
steveschult <steveschultz8864@gmail.com>
2024-01-03 10:07:35 +0800

    feat: draw basic rate

--- extensions/database/src/ui/views/TableView/cells/rate-cell.tsx
+++ extensions/database/src/ui/views/TableView/cells/rate-cell.tsx
@@ -17,8 +17,30 @@ export const RateCellRenderer: CustomRenderer<RateCell> = {
   kind: GridCellKind.Custom,
   isMatch: (c): c is RateCell => (c.data as any).kind === 'rate-cell',
   draw: (args, cell) => {
-    const { data = '' } = cell.data
-    drawTextCell(args, data)
+    const { ctx, rect } = args
+
+    const x = rect.x + rect.width / 2
+    const y = rect.y + rect.height / 2
+    const size = (Math.min(rect.width, rect.height) / 2) * 0.8
+
+    ctx.save()
+    ctx.beginPath()
+    ctx.translate(x, y)
+    ctx.rotate(0.02)
+    for (let i = 0; i < 5; i++) {
+      ctx.rotate((Math.PI / 180) * 72)
+      ctx.lineTo(0, 0 - size / 2)
+      ctx.rotate((Math.PI / 180) * 72)
+      ctx.lineTo(0, 0 - size)
+    }
+
+    ctx.fillStyle = 'gray'
+    ctx.strokeStyle = 'gray'
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.restore()
+
     return true
   },
   provideEditor: () => ({
@@ -28,8 +50,6 @@ export const RateCellRenderer: CustomRenderer<RateCell> = {
       return (
         <TextCellEntry
           highlight={true}
-          // autoFocus={false}
-          // disabled={true}
           value={value.data.data ?? ''}
           onChange={(e) => {
             onChange({
