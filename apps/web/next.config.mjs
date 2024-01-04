33e0237f1200d59c10a3173b99a1a2349b870199
0xzion <0xyz.penx@gmail.com>
2024-01-04 10:50:43 +0800

    refactor: improve Dockerfile

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -16,6 +16,7 @@ const withPWA = NextPWA({
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
   transpilePackages: [
     '@penx/api',
     '@penx/app',
