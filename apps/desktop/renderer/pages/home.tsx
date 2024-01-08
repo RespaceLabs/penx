35af2b63e5d9602cba1d1912931f3c4cd4dbd4a6
0xzion <0xyz.penx@gmail.com>
2024-01-08 21:36:54 +0800

    feat: init deskop

---
+++ apps/desktop/renderer/pages/home.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function HomePage() {
+  const [message, setMessage] = React.useState('No message found')
+
+  React.useEffect(() => {
+    window.ipc.on('message', (message: string) => {
+      setMessage(message)
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (basic-lang-typescript)</title>
+      </Head>
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -
+          <Link href="/next">
+            <a>Go to next page</a>
+          </Link>
+        </p>
+        <Image
+          src="/images/logo.png"
+          alt="Logo image"
+          width="256px"
+          height="256px"
+        />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            window.ipc.send('message', 'Hello')
+          }}
+        >
+          Test IPC~~~
+        </button>
+        <p>{message}</p>
+      </div>
+    </React.Fragment>
+  )
+}
