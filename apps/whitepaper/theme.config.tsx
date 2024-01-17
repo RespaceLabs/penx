45827a4908f9126dcf507c38355b183f3fbced01
0xzion <0xzion.penx@gmail.com>
2024-01-17 09:51:49 +0800

    docs: add whitepaper

---
+++ apps/whitepaper/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>PenX</span>,
+  project: {
+    link: 'https://github.com/penxio/penx',
+  },
+  chat: {
+    link: 'https://discord.gg/nyVpH9njDu',
+  },
+  docsRepositoryBase: 'https://github.com/penxio/penx',
+  footer: {
+    text: 'PenX Docs',
+  },
+}
+
+export default config
