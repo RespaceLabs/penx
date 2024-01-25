54844e693b81c621f914ab93f21c09cfdf838a54
0xzion <0xzion.penx@gmail.com>
2024-01-25 21:40:56 +0800

    feat: improve login in desktop

--- apps/desktop/src/pages/_app.tsx
+++ apps/desktop/src/pages/_app.tsx
@@ -2,14 +2,15 @@ import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'uikit'
 import { initFower } from '@penx/app'
+import { TrpcProvider } from '@penx/trpc-client'
 
 initFower()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TrpcProvider>
       <ToastContainer position="bottom-right" />
       <Component {...pageProps} />
-    </>
+    </TrpcProvider>
   )
 }
--- apps/desktop/src/pages/editor.tsx
+++ apps/desktop/src/pages/editor.tsx
@@ -2,15 +2,16 @@ import { Box } from '@fower/react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Button } from 'uikit'
+import { trpc } from '@penx/trpc-client'
 import { ClientOnly } from '~/components/ClientOnly'
 
 export default function PageEditor() {
   const { push } = useRouter()
+  const { isLoading, data } = trpc.space.mySpaces.useQuery()
+  console.log('isLoading', isLoading, 'data', data)
+
   return (
     <>
-      <Head>
-        <title>PenX</title>
-      </Head>
       <ClientOnly>
         <Box>Editor</Box>
         <Button
