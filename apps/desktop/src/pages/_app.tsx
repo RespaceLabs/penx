e9d71eb9a7402c04b8d7f0f9ba469b5aee2cc78a
0xzion <0xzion.penx@gmail.com>
2024-01-26 18:00:57 +0800

    feat: can logout in desktop

--- apps/desktop/src/pages/_app.tsx
+++ apps/desktop/src/pages/_app.tsx
@@ -1,12 +1,30 @@
 import '~/styles/globals.css'
+import { useEffect } from 'react'
+import { set } from 'idb-keyval'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import { ToastContainer } from 'uikit'
-import { initFower } from '@penx/app'
+import { appEmitter, initFower } from '@penx/app'
+import { PENX_SESSION_USER } from '@penx/constants'
+import { store } from '@penx/store'
 import { TrpcProvider } from '@penx/trpc-client'
 
 initFower()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { push } = useRouter()
+  useEffect(() => {
+    const handleSignOut = () => {
+      set(PENX_SESSION_USER, null)
+      store.setToken(null as any)
+      push('/')
+    }
+
+    appEmitter.on('SIGN_OUT', handleSignOut)
+    return () => {
+      appEmitter.off('SIGN_OUT', handleSignOut)
+    }
+  }, [])
   return (
     <TrpcProvider>
       <ToastContainer position="bottom-right" />
--- apps/desktop/src/pages/editor.tsx
+++ apps/desktop/src/pages/editor.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { get } from 'idb-keyval'
+import { useAtom } from 'jotai'
+import { useRouter } from 'next/router'
 import { EditorApp } from '@penx/app'
 import { PENX_SESSION_USER } from '@penx/constants'
 import { SessionProvider } from '@penx/session'
@@ -7,10 +9,18 @@ import { SessionProvider } from '@penx/session'
 export default function PageEditor() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState<any>(null)
+  const { push } = useRouter()
+
   useEffect(() => {
     get(PENX_SESSION_USER).then((user) => {
-      setUser(user)
-      setLoading(false)
+      console.log('======user:', user)
+      if (!user) {
+        push('/')
+        // setLoading(false)
+      } else {
+        setUser(user)
+        setLoading(false)
+      }
     })
   }, [])
 
