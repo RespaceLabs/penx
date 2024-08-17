1056aeaa5c386b1bb975a90cab1dea9000205efe
steveschult <steveschultz8864@gmail.com>
2024-08-17 22:58:22 +0800

    feat: sell test

--- components/spaceToken/SellBtn.tsx
+++ components/spaceToken/SellBtn.tsx
@@ -10,6 +10,8 @@ import { WalletConnectButton } from '../WalletConnectButton'
 import { Space } from '@/app/~/space/[id]/hooks/useSpace'
 import { Address } from 'viem'
 import { spaceAbi } from '@/lib/abi/indieX'
+import { extractErrorMessage } from '@/lib/extractErrorMessage'
+import { erc20Abi } from '@/lib/abi'
 
 interface Props {
   ethAmount: string;
@@ -34,18 +36,33 @@ export const SellBtn = ({
   const balance = useSpaceTokenBalance()
 
   const onSell = async () => {
-    const value = precision.token(parseFloat(purchasedAmount), 18)
-    const hash = await writeContractAsync({
-      address: space.spaceAddress as Address,
-      abi: spaceAbi,
-      functionName: 'sell',
-      args: [value]
-    })
+    const value = precision.token(purchasedAmount, 18)
+    try {
+      const approveTx = await writeContractAsync({
+        address: space.spaceAddress as Address,
+        abi: erc20Abi,
+        functionName: 'approve',
+        args: [space.spaceAddress as Address, value],
+      })
 
-    await waitForTransactionReceipt(wagmiConfig, { hash })
-    await balance.refetch()
-    handleSwap()
-    toast.success(`${space?.name} sell successfully!`)
+      await waitForTransactionReceipt(wagmiConfig, { hash: approveTx })
+
+      const hash = await writeContractAsync({
+        address: space.spaceAddress as Address,
+        abi: spaceAbi,
+        functionName: 'sell',
+        args: [value]
+      })
+
+      await waitForTransactionReceipt(wagmiConfig, { hash })
+      await balance.refetch()
+      handleSwap()
+      toast.success(`${space?.name} sell successfully!`)
+    } catch (error) {
+      const msg = extractErrorMessage(error)
+      console.log('%c=error', 'color:red', error, 'msg', msg)
+      toast.error(msg)
+    }
   }
 
   return (
