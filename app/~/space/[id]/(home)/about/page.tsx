93deca616ed189691b374e9bd97f810d2a1fbf7f
steveschult <steveschultz8864@gmail.com>
2024-08-31 13:53:59 +0800

    feat: edit space about

--- app/~/space/[id]/(home)/about/page.tsx
+++ app/~/space/[id]/(home)/about/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { useSpace } from "@/hooks/useSpace"
+import { About } from "./about"
+
 export default function Page() {
-  return (
-    <div>
-      test
-    </div>
-  )
+  const { space } = useSpace()
+
+  if (!space || !space.about) return <div className="text-neutral-500">
+    No about yet!
+  </div>
+
+  return <About />
 }
