b14603d4d75e11f28d0d9dba3a863a7d2b536ad8
0xzio <zio.penx@gmail.com>
2024-10-04 23:29:29 +0800

    feat: can render member list

---
+++ components/MemberList.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { spaceAbi } from '@/common/abi'
+import { Subscription } from '@/common/Subscription'
+import { Address } from 'viem'
+import { useReadContract } from 'wagmi'
+import { UserAvatar } from './UserAvatar'
+
+export function MemberList() {
+  const { data = [], isLoading } = useReadContract({
+    abi: spaceAbi,
+    address: process.env.NEXT_PUBLIC_SPACE_ID as Address,
+    functionName: 'getSubscriptions',
+  })
+
+  console.log('======data:', data)
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div className="grid gap-4">
+      {data.map((member, index) => {
+        const subscription = new Subscription({
+          planId: member.planId,
+          account: member.account,
+          amount: BigInt(member.amount),
+          duration: BigInt(member.duration),
+          startTime: BigInt(member.startTime),
+        })
+        return (
+          <div key={index} className="flex items-center justify-between gap-2">
+            <div className="flex items-center gap-2">
+              <UserAvatar address={member.account} />
+              <div> {member.account}</div>
+            </div>
+            <div>
+              <span className="font-bold">{subscription.daysFormatted}</span> days
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  { href: '/members', title: 'Members' },
+]
+
+export default headerNavLinks
