29c5b10e0ae08da9ef0d9e22244073a784d2a022
0xzio <zio.penx@gmail.com>
2025-02-24 16:06:51 +0800

    feat: improve content

--- components/theme-ui/ContentRender/SlateContent.tsx
+++ components/theme-ui/ContentRender/SlateContent.tsx
@@ -20,6 +20,8 @@ import {
   ELEMENT_UL,
 } from '@/lib/constants'
 import { cn, getUrl } from '@/lib/utils'
+import { slug } from 'github-slugger'
+import { Node } from 'slate'
 import { Editable } from 'slate-react'
 import { Leaf } from './Leaf'
 
@@ -30,6 +32,8 @@ export function SlateContent() {
       renderLeaf={(props) => <Leaf {...props} />}
       renderElement={({ attributes, children, element }) => {
         // console.log('=======element.type:', element.type)
+        const text = Node.string(element)
+        const id = slug(text)
 
         switch (element.type) {
           case ELEMENT_P:
@@ -44,7 +48,7 @@ export function SlateContent() {
                   }}
                   {...attributes}
                 >
-                  <li className="[&>*]:inline-flex">{children}</li>
+                  <li className="[&>*]:inline">{children}</li>
                 </ul>
               )
             }
@@ -54,21 +58,49 @@ export function SlateContent() {
               </div>
             )
           case ELEMENT_H1:
-            return <h1 {...attributes}>{children}</h1>
+            return (
+              <h1 id={id} {...attributes}>
+                {children}
+              </h1>
+            )
           case ELEMENT_H2:
-            return <h2 {...attributes}>{children}</h2>
+            return (
+              <h2 id={id} {...attributes}>
+                {children}
+              </h2>
+            )
           case ELEMENT_H3:
-            return <h3 {...attributes}>{children}</h3>
+            return (
+              <h3 id={id} {...attributes}>
+                {children}
+              </h3>
+            )
           case ELEMENT_H4:
-            return <h4 {...attributes}>{children}</h4>
+            return (
+              <h4 id={id} {...attributes}>
+                {children}
+              </h4>
+            )
           case ELEMENT_H5:
-            return <h5 {...attributes}>{children}</h5>
+            return (
+              <h5 id={id} {...attributes}>
+                {children}
+              </h5>
+            )
           case ELEMENT_H6:
-            return <h6 {...attributes}>{children}</h6>
+            return (
+              <h6 id={id} {...attributes}>
+                {children}
+              </h6>
+            )
           case ELEMENT_HR:
             return <hr {...attributes}></hr>
           case ELEMENT_UL:
-            return <ul {...attributes}>{children}</ul>
+            return (
+              <ul className="bg-red-300" {...attributes}>
+                {children}
+              </ul>
+            )
           case ELEMENT_OL:
             return <ol {...attributes}>{children}</ol>
           case 'numbered-list':
@@ -76,7 +108,11 @@ export function SlateContent() {
           case ELEMENT_LI:
             return <>{children}</>
           case ELEMENT_LIC:
-            return <li {...attributes}>{children}</li>
+            return (
+              <li className="inline [&>*]:inline bg-amber-200" {...attributes}>
+                {children}
+              </li>
+            )
           case ELEMENT_TODO:
             const checked = (element as any).checked
             return (
@@ -96,8 +132,11 @@ export function SlateContent() {
               </div>
             )
           case ELEMENT_IMG:
+            const width = (element as any).width
             return (
               <img
+                className="mx-auto"
+                style={{ width: width ? width : 'auto' }}
                 src={getUrl((element as any).url)}
                 alt=""
                 {...attributes}
